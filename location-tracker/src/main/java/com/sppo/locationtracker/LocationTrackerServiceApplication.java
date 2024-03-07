15cca942b401f58464255785cec339a1efc6120b
FellipeToledo <fellipetoledo.dev@gmail.com>
2024-03-07 15:18:49 -0300

    feat: initial commit location tracker service

---
+++ location-tracker/src/main/java/com/sppo/locationtracker/LocationTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sppo.locationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationTrackerServiceApplication.class, args);
+	}
+
+}
